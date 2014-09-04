ccb66da000a0c4338e7269711c218b4e148de816
ayushgaud <ayush.gaud@gmail.com>
2014-09-04 23:20:24 +0530

    Create mpu6050_final.ino

---
+++ examples/mpu6050_final.ino
@@ -0,0 +1,126 @@
+#include "mpu6050.h"
+#include<Wire.h>
+
+
+void setup()
+{      
+       
+  int error;
+  uint8_t c;
+
+
+  Serial.begin(115200);
+  Serial.println(F("InvenSense MPU-6050"));
+
+
+  // Initialize the 'Wire' class for the I2C-bus.
+  Wire.begin();
+
+
+  // default at power-up:
+  //    Gyro at 250 degrees second
+  //    Acceleration at 2g
+  //    Clock source at internal 8MHz
+  //    The device is in sleep mode.
+  //
+
+  error = MPU6050_read (MPU6050_WHO_AM_I, &c, 1);
+  Serial.print(F("WHO_AM_I : "));
+  Serial.print(c,HEX);
+  Serial.print(F(", error = "));
+  Serial.println(error,DEC);
+
+  // According to the datasheet, the 'sleep' bit
+  // should read a '1'. But I read a '0'.
+  // That bit has to be cleared, since the sensor
+  // is in sleep mode at power-up. Even if the
+  // bit reads '0'.
+  error = MPU6050_read (MPU6050_PWR_MGMT_2, &c, 1);
+  Serial.print(F("PWR_MGMT_2 : "));
+  Serial.print(c,HEX);
+  Serial.print(F(", error = "));
+  Serial.println(error,DEC);
+
+
+  // Clear the 'sleep' bit to start the sensor.
+  MPU6050_write_reg (MPU6050_PWR_MGMT_1, 0);
+}
+
+
+void loop()
+{
+  int error;
+  double dT;
+  accel_t_gyro_union accel_t_gyro;
+
+
+  Serial.println(F(""));
+  Serial.println(F("MPU-6050"));
+
+  // Read the raw values.
+  // Read 14 bytes at once, 
+  // containing acceleration, temperature and gyro.
+  // With the default settings of the MPU-6050,
+  // there is no filter enabled, and the values
+  // are not very stable.
+  error = MPU6050_read (MPU6050_ACCEL_XOUT_H, (uint8_t *) &accel_t_gyro, sizeof(accel_t_gyro));
+  Serial.print(F("Read accel, temp and gyro, error = "));
+  Serial.println(error,DEC);
+
+
+  // Swap all high and low bytes.
+  // After this, the registers values are swapped, 
+  // so the structure name like x_accel_l does no 
+  // longer contain the lower byte.
+  uint8_t swap;
+  #define SWAP(x,y) swap = x; x = y; y = swap
+
+  SWAP (accel_t_gyro.reg.x_accel_h, accel_t_gyro.reg.x_accel_l);
+  SWAP (accel_t_gyro.reg.y_accel_h, accel_t_gyro.reg.y_accel_l);
+  SWAP (accel_t_gyro.reg.z_accel_h, accel_t_gyro.reg.z_accel_l);
+  SWAP (accel_t_gyro.reg.t_h, accel_t_gyro.reg.t_l);
+  SWAP (accel_t_gyro.reg.x_gyro_h, accel_t_gyro.reg.x_gyro_l);
+  SWAP (accel_t_gyro.reg.y_gyro_h, accel_t_gyro.reg.y_gyro_l);
+  SWAP (accel_t_gyro.reg.z_gyro_h, accel_t_gyro.reg.z_gyro_l);
+
+
+  // Print the raw acceleration values
+
+  Serial.print(F("accel x,y,z: "));
+  Serial.print(accel_t_gyro.value.x_accel, DEC);
+  Serial.print(F(", "));
+  Serial.print(accel_t_gyro.value.y_accel, DEC);
+  Serial.print(F(", "));
+  Serial.print(accel_t_gyro.value.z_accel, DEC);
+  Serial.println(F(""));
+
+
+  // The temperature sensor is -40 to +85 degrees Celsius.
+  // It is a signed integer.
+  // According to the datasheet: 
+  //   340 per degrees Celsius, -512 at 35 degrees.
+  // At 0 degrees: -512 - (340 * 35) = -12412
+
+  Serial.print(F("temperature: "));
+  dT = ( (double) accel_t_gyro.value.temperature + 12412.0) / 340.0;
+  Serial.print(dT, 3);
+  Serial.print(F(" degrees Celsius"));
+  Serial.println(F(""));
+
+
+  // Print the raw gyro values.
+
+  Serial.print(F("gyro x,y,z : "));
+  Serial.print(accel_t_gyro.value.x_gyro, DEC);
+  Serial.print(F(", "));
+  Serial.print(accel_t_gyro.value.y_gyro, DEC);
+  Serial.print(F(", "));
+  Serial.print(accel_t_gyro.value.z_gyro, DEC);
+  Serial.print(F(", "));
+  Serial.println(F(""));
+
+  delay(1000);
+}
+
+
+
